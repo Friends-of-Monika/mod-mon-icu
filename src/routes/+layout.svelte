a856c60fbeb536685b5dc26e0936994ef83c881a
Herman S. <dreamscache.d@gmail.com>
2024-11-22 18:02:54 +0100

    Enable page titles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
+	import { page } from "$app/stores";
 	import "../app.css";
+
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	{#if $page.data.title != null}
+		<title>{$page.data.title} • mod.mon.icu</title>
+	{/if}
+</svelte:head>
 
 {@render children()}
