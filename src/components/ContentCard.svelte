79a36c48db3e782620ab41c8312d2d4aed151c15
Herman S. <dreamscache.d@gmail.com>
2024-11-23 13:25:50 +0100

    Add css to tags, add more content

--- src/components/ContentCard.svelte
+++ src/components/ContentCard.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import IconLink from "$components/icon/IconLink.svelte";
 	import ContentAttribution from "$components/ContentAttribution.svelte";
-
 	import {
 		ArrowDownTray,
 		ExclamationTriangle,
 		Home,
 		QuestionMarkCircle
 	} from "svelte-hero-icons";
-	import type { Content } from "$lib/content";
+
+	import type { Content, tags } from "$lib/content";
+	import ContentTag from "./ContentTag.svelte";
 
 	interface Props {
 		content: Content;
@@ -18,48 +19,64 @@
 </script>
 
 <div
-	class="flex select-none flex-col rounded-md bg-white p-4 text-black drop-shadow-md dark:bg-neutral-700 dark:text-white">
+	class="flex select-none flex-col justify-between gap-2 rounded-md bg-white p-4 text-black drop-shadow-md dark:bg-neutral-700 dark:text-white">
 	<div class="flex flex-row flex-nowrap">
 		<div class="flex-grow">
-			<h1 class="text-lg">{content.name}</h1>
-			<span class="mb-1 text-xs">
-				<ContentAttribution creators={content.creators} />
-			</span>
-			<p class="text-sm text-neutral-700 dark:text-neutral-400">
-				{content.description}
-			</p>
+			{@render title()}
 		</div>
-
 		<div class="flex flex-shrink flex-col flex-nowrap gap-2 lg:flex-row">
-			{#if content.links.homepage}
-				<IconLink
-					class="h-6 w-6 text-neutral-700 dark:text-white"
-					href={content.links.homepage}
-					src={Home}
-					alt="Homepage" />
-			{/if}
-
-			{#if content.links.support}
-				<IconLink
-					class="h-6 w-6 text-neutral-700 dark:text-white"
-					href={content.links.support}
-					src={QuestionMarkCircle}
-					alt="Get support" />
-			{/if}
-
-			{#if content.links.issues}
-				<IconLink
-					class="h-6 w-6 text-neutral-700 dark:text-white"
-					href={content.links.issues}
-					src={ExclamationTriangle}
-					alt="Report an issue" />
-			{/if}
-
-			<IconLink
-				class="h-6 w-6 text-neutral-700 dark:text-white"
-				href={content.links.download}
-				src={ArrowDownTray}
-				alt="Download" />
+			{@render links()}
 		</div>
 	</div>
+	<div class="flex flex-row flex-wrap gap-2">
+		{@render tags()}
+	</div>
 </div>
+
+{#snippet title()}
+	<h1 class="text-lg">{content.name}</h1>
+	<span class="mb-1 text-xs">
+		<ContentAttribution creators={content.creators} />
+	</span>
+	<p class="text-sm text-neutral-700 dark:text-neutral-400">
+		{content.description}
+	</p>
+{/snippet}
+
+{#snippet links()}
+	{#if content.links.homepage}
+		<IconLink
+			class="h-6 w-6 text-neutral-700 dark:text-white"
+			href={content.links.homepage}
+			src={Home}
+			alt="Homepage" />
+	{/if}
+
+	{#if content.links.support}
+		<IconLink
+			class="h-6 w-6 text-neutral-700 dark:text-white"
+			href={content.links.support}
+			src={QuestionMarkCircle}
+			alt="Get support" />
+	{/if}
+
+	{#if content.links.issues}
+		<IconLink
+			class="h-6 w-6 text-neutral-700 dark:text-white"
+			href={content.links.issues}
+			src={ExclamationTriangle}
+			alt="Report an issue" />
+	{/if}
+
+	<IconLink
+		class="h-6 w-6 text-neutral-700 dark:text-white"
+		href={content.links.download}
+		src={ArrowDownTray}
+		alt="Download" />
+{/snippet}
+
+{#snippet tags()}
+	{#each content.tags as tag}
+		<ContentTag {tag} />
+	{/each}
+{/snippet}
---
+++ src/components/ContentTag.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Tag } from "$lib/content";
+
+	interface Props {
+		tag: Tag;
+	}
+
+	const { tag }: Props = $props();
+</script>
+
+<span class="rounded-full px-2 py-1 text-xs {tag.css}">{tag.name}</span>
