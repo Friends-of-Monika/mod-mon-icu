14a1d2ad17bfd640a391ca90398eaab023d6f95d
Herman S. <dreamscache.d@gmail.com>
2025-01-07 09:58:23 +0100

    Drawer on tablet too

--- src/components/layouts/SidebarLayout.svelte
+++ src/components/layouts/SidebarLayout.svelte
@@ -17,13 +17,13 @@
 <div class="flex flex-col md:flex-row">
 	<nav
 		class={clsx(
-			"fixed z-20 h-screen w-4/5 bg-neutral-200 p-4 drop-shadow-md transition duration-100 md:static md:h-[initial] md:w-1/4 md:drop-shadow-none lg:w-1/6 dark:bg-neutral-900",
-			{ "-translate-x-full": !drawerOpen, "md:translate-x-0": !drawerOpen }
+			"fixed z-20 h-screen w-4/5 bg-neutral-200 p-4 drop-shadow-md transition duration-100 md:w-2/5 md:drop-shadow-none lg:static lg:h-[initial] lg:w-1/6 dark:bg-neutral-900",
+			{ "-translate-x-full": !drawerOpen, "lg:translate-x-0": !drawerOpen }
 		)}>
 		<!-- Mobile only: button to toggle drawer -->
 		<button
 			class={clsx(
-				"absolute bottom-[calc(env(safe-area-inset-bottom)+2rem)] left-full rounded-r-full p-4 text-neutral-400 transition duration-100 md:hidden",
+				"absolute bottom-[calc(env(safe-area-inset-bottom)+2rem)] left-full rounded-r-full p-4 text-neutral-400 transition duration-100 lg:hidden",
 				{
 					"bg-neutral-200/50 dark:bg-neutral-900/50": !drawerOpen,
 					"bg-neutral-200 dark:bg-neutral-900": drawerOpen
@@ -46,14 +46,14 @@
 		<!-- svelte-ignore a11y_click_events_have_key_events -->
 		<!-- svelte-ignore a11y_no_static_element_interactions -->
 		<div
-			class="fixed z-10 h-screen w-full bg-white/50 md:hidden dark:bg-black/50"
+			class="fixed z-10 h-screen w-full bg-white/50 lg:hidden dark:bg-black/50"
 			in:fade={{ duration: 100 }}
 			out:fade={{ duration: 100 }}
 			onclick={() => (drawerOpen = false)}>
 		</div>
 	{/if}
 
-	<main class="min-h-screen w-full bg-neutral-100 p-4 md:w-5/6 dark:bg-neutral-800">
+	<main class="min-h-screen w-full bg-neutral-100 p-4 dark:bg-neutral-800">
 		{@render main()}
 	</main>
 </div>
